a65dfe6eceebe2410b8929240c5e986ec699bd71
hainguyen-ht <hainguyen.microsoft@gmail.com>
2021-02-25 17:31:48 +0700

    update db

--- app/database.php
+++ app/database.php
@@ -1,8 +1,13 @@
 <?php
-	$db_host = 'localhost';
-	$db_user = 'root';
-	$db_pass = '';
-	$db_name = 'todo_db';
+	// $db_host = 'localhost';
+	// $db_user = 'root';
+	// $db_pass = '';
+	// $db_name = 'todo_db';
+
+	$db_host = 'remotemysql.com';
+	$db_user = 'FhLHgblj4Y';
+	$db_pass = 'z8OFyA56lg';
+	$db_name = 'FhLHgblj4Y';
 
 	$conn = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 	if(!$conn){
